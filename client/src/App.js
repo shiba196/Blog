31f0ead83588f81448c00e0839b9c99146ca3d41
Shibam Acharya <shibamacharya@gmail.com>
2023-08-11 19:44:50 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Spiderman</h2>
+          <p className="info">
+           
+          </p>
+          <p>Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He has been featured in comic books, television shows, films, video games, novels, and plays.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Spiderman</h2>
+          <p>Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He has been featured in comic books, television shows, films, video games, novels, and plays.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Spiderman</h2>
+          <p>Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He has been featured in comic books, television shows, films, video games, novels, and plays.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
